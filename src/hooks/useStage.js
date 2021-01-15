3df354df343c9a15362c337ee4eec7dea523e7cd
MariaMoreno <maru_200202@yahoo.com.ar>
2021-01-15 13:23:22 -0300

    tretrom

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,9 +1,35 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 import {createStage} from '../gameHelpers';
 
-export const useStage =()=>{
+export const useStage =(player, resetPlayer)=>{
     const [stage, setStage] = useState(createStage());
+    
+    useEffect(() => {
+       const updateStage = prevStage =>{
+        //montamos el escenario
+        const newStage = prevStage.map(row =>
+          row.map(cell => (cell[1] === 'clear'  ? [0, 'clear'] : cell)),  
+        );
+
+        // dibuja el tetromino
+        player.tetromino.forEach((row, y) => {
+            row.forEach((value, x) => {
+                if(value !== 0){
+                    newStage[ y + player.pos.y][x + player.pos.x] = [
+                        value,
+                        `${player.collided ? 'merged' : 'clear'}`,
+                    ]
+                }
+            })
+        })
+
+       }
+
+
+    setStage(prev => updateStage(prev))
+
+    }, [])
 
     return[stage,setStage]; 
 }
